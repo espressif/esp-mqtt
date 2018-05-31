c83a784e27df1861e53d981a0101946b040c318f
Tony <tony@foobot.io>
2018-05-31 13:54:56 +0200

    Adding core selection in KConfig and starting thread with those parameters

--- mqtt_client.c
+++ mqtt_client.c
@@ -736,9 +736,18 @@ esp_err_t esp_mqtt_client_start(esp_mqtt_client_handle_t client)
         ESP_LOGE(TAG, "Client has started");
         return ESP_FAIL;
     }
-    if (xTaskCreate(esp_mqtt_task, "mqtt_task", client->config->task_stack, client, client->config->task_prio, NULL) != pdTRUE) {
-        ESP_LOGE(TAG, "Error create mqtt task");
-        return ESP_FAIL;
+    if(MQTT_CORE_SELECTION_ENABLED){
+    	ESP_LOGD(TAG, "Core selection enabled on %u", MQTT_TASK_CORE);
+		if (xTaskCreatePinnedToCore(esp_mqtt_task, "mqtt_task", client->config->task_stack, client, client->config->task_prio, NULL, MQTT_TASK_CORE) != pdTRUE) {
+			ESP_LOGE(TAG, "Error create mqtt task");
+			return ESP_FAIL;
+		}
+    } else{
+    	ESP_LOGD(TAG, "Core selection disabled");
+		if (xTaskCreate(esp_mqtt_task, "mqtt_task", client->config->task_stack, client, client->config->task_prio, NULL) != pdTRUE) {
+			ESP_LOGE(TAG, "Error create mqtt task")	;
+			return ESP_FAIL;
+		}
     }
     return ESP_OK;
 }
