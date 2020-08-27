04b3f8c8a24e288e66a19ec462f6ed9e233bd98d
Dmitriy Shilin <dshil@fastmail.com>
2020-08-27 16:34:22 +0300

    Allow to configure network timeout

--- include/mqtt_client.h
+++ include/mqtt_client.h
@@ -177,6 +177,7 @@ typedef struct {
     const struct psk_key_hint* psk_hint_key;     /*!< Pointer to PSK struct defined in esp_tls.h to enable PSK authentication (as alternative to certificate verification). If not NULL and server/client certificates are NULL, PSK is enabled */
     bool          use_global_ca_store;      /*!< Use a global ca_store for all the connections in which this bool is set. */
     int reconnect_timeout_ms;               /*!< Reconnect to the broker after this value in miliseconds if auto reconnect is not disabled */
+    int network_timeout_ms;                 /*!< Abort network operation if it is not completed after this value, in milliseconds */
     const char **alpn_protos;               /*!< NULL-terminated list of supported application protocols to be used for ALPN */
     const char *clientkey_password;         /*!< Client key decryption password string */
     int clientkey_password_len;             /*!< String length of the password pointed to by clientkey_password */
