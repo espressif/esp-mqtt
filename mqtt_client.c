espressif/esp-mqtt

commit 5e17dcaeb257f847386478119935ecd360f47377

MQTT: Fix esp_mqtt_client_stop deadlock
esp_mqtt_client_stop would lead to a deadlock (with itself) when called
from the event handler.

Updated comments to reflect that some functions should not be called from
the event handler.

Closes https://github.com/espressif/esp-mqtt/issues/163

mqtt_client.c
@@ -1481,6 +1481,13 @@ esp_err_t esp_mqtt_client_stop(esp_mqtt_client_handle_t client)
 {
     MQTT_API_LOCK(client);
     if (client->run) {
+        /* A running client cannot be stopped from the MQTT task/event handler */
+        TaskHandle_t running_task = xTaskGetCurrentTaskHandle();
+        if (running_task == client->task_handle) {
+            ESP_LOGE(TAG, "Client cannot be stopped from MQTT task");
+            return ESP_FAIL;
+        }
+
         // Only send the disconnect message if the client is connected
         if(client->state == MQTT_STATE_CONNECTED) {
             // Notify the broker we are disconnecting

include/mqtt_client.h
@@ -239,6 +239,9 @@ esp_err_t esp_mqtt_client_disconnect(esp_mqtt_client_handle_t client);
 /**
  * @brief Stops mqtt client tasks
  *
+ *  * Notes:
+ *  - Cannot be called from the mqtt event handler
+ *
  * @param client    mqtt client handle
  *
  * @return ESP_OK on success
@@ -305,6 +308,9 @@ int esp_mqtt_client_publish(esp_mqtt_client_handle_t client, const char *topic, 
 
 /**
  * @brief Destroys the client handle
+ *
+ * Notes:
+ *  - Cannot be called from the mqtt event handler
  *
  * @param client    mqtt client handle
  *

commit 845c2a3a1e112af5dbe5f3a9ee8f6adb92a03757

mqtt: Fix missing MQTT_API_UNLOCK in esp_mqtt_client_stop error path
Fixes: 5e17dcaeb257 ("MQTT: Fix esp_mqtt_client_stop deadlock")
Signed-off-by: Axel Lin <axel.lin@ingics.com>

mqtt_client.c
@@ -1494,6 +1494,7 @@ esp_err_t esp_mqtt_client_stop(esp_mqtt_client_handle_t client)
         /* A running client cannot be stopped from the MQTT task/event handler */
         TaskHandle_t running_task = xTaskGetCurrentTaskHandle();
         if (running_task == client->task_handle) {
+            MQTT_API_UNLOCK(client);
             ESP_LOGE(TAG, "Client cannot be stopped from MQTT task");
             return ESP_FAIL;
         }