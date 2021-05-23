a00a3134c67657949ab7ab5317133a49a855fe30
David Cermak <cermak@espressif.com>
2021-05-23 15:43:07 +0200

    Add support for Retain flag in messages posted by events
    Closes https://github.com/espressif/esp-mqtt/issues/193

--- mqtt_client.c
+++ mqtt_client.c
@@ -923,6 +923,7 @@ static esp_err_t deliver_publish(esp_mqtt_client_handle_t client)
     }
 
     // post data event
+    client->event.retain = mqtt_get_retain(msg_buf);
     client->event.msg_id = mqtt_get_id(msg_buf, msg_data_len);
     client->event.total_data_len = msg_data_len + msg_total_len - msg_read_len;
 
